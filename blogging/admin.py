658cd77950f85c39b8269d333fa55f2549ea46f5
bubblecfd <taoyeh@gmail.com>
2021-02-21 22:45:36 -0800

    Lesson 7 assignment: fixing the blog

--- blogging/admin.py
+++ blogging/admin.py
@@ -2,5 +2,20 @@ from django.contrib import admin
 from blogging.models import Post, Category
 
 # and a new admin registration
-admin.site.register(Post)
-admin.site.register(Category)
+
+class CategoryInline(admin.TabularInline):
+    model = Category.posts.through
+
+class PostAdmin(admin.ModelAdmin):
+    inlines = [
+        CategoryInline,
+    ]
+
+class CategoryAdmin(admin.ModelAdmin):
+    #inlines = [
+    #    CategoryInline,
+    #]
+    exclude = ('posts',)
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Post, PostAdmin)
